f46d08d43e30b764165e5dd5274748a3a2a9fa4d
Olga-Zas <hohol02@mail.ua>
2016-06-01 01:26:44 +0300

    Add files via upload

---
+++ Site/CRUD/add.php
@@ -0,0 +1,88 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+	<title>Додати дані</title>
+</head>
+
+<body>
+<?php
+//including the database connection file
+include_once("config.php");
+if(isset($_POST['Submit'])) {	
+	$name = mysql_escape_string($_POST['name']);
+	$surname = mysql_escape_string($_POST['surname']);
+	$middlename = mysql_escape_string($_POST['middlename']);
+	$login = mysql_escape_string($_POST['login']);
+	$country = mysql_escape_string($_POST['country']);
+	$city = mysql_escape_string($_POST['city']);
+	$email = mysql_escape_string($_POST['email']);
+	$phone = mysql_escape_string($_POST['phone']);
+	$password = mysql_escape_string($_POST['password']);
+		
+	// checking empty fields
+	if(empty($name) || empty($surname) || empty($middlename) || empty($login) || empty($country) || empty($city) || empty($email) || empty($phone) || empty($password)) 
+	{
+				
+		if(empty($name)) 
+		{
+			echo "<font color='red'>Name field is empty.</font><br/>";
+		}
+		
+		if(empty($surname)) 
+		{
+			echo "<font color='red'>Surname field is empty.</font><br/>";
+		}
+		
+		if(empty($middlename)) 
+		{
+			echo "<font color='red'>Middlename field is empty.</font><br/>";
+		}
+		
+		if(empty($login)) 
+		{
+			echo "<font color='red'>Login field is empty.</font><br/>";
+		}
+		
+		if(empty($country)) 
+		{
+			echo "<font color='red'>Country field is empty.</font><br/>";
+		}
+		
+		if(empty($city)) 
+		{
+			echo "<font color='red'>City field is empty.</font><br/>";
+		}
+		
+		if(empty($email)) 
+		{
+			echo "<font color='red'>Email field is empty.</font><br/>";
+		}
+		
+		if(empty($phone)) 
+		{
+			echo "<font color='red'>Phone field is empty.</font><br/>";
+		}
+		
+		if(empty($password)) 
+		{
+			echo "<font color='red'>Password field is empty.</font><br/>";
+		}
+		
+		//link to the previous page
+		echo "<br/><a href='javascript:self.history.back();'>Go Back</a>";
+	} 
+	else 
+	{ 
+		// if all the fields are filled (not empty) 
+			
+		//insert data to database	
+		$result = mysql_query("INSERT INTO users(name,surname,middlename,login,country,city,email,phone,password) VALUES ('$name','$surname','$middlename','$login','$country','$city','$email','$phone','$password')");
+		
+		//display success message
+		echo "<font color='green'>Дані додано успішно!";
+		echo "<br/><a href='index.php'>Переглянути результат</a>";
+	}
+}
+?>
+</body>
+</html>
